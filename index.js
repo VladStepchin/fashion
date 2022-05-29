cd2ab8186ea9c45dc604e4aec5665eff549460b3
Vlad Stepchyn <v.stepchyn@viseven.com>
2022-05-29 23:43:02 +0300

    update

--- index.js
+++
@@ -1,9 +0,0 @@
-const audio = new Audio("song.mp3")
-const button = document.getElementById("secret")
-const alina = document.getElementById("alina")
-
-button.addEventListener("click", () => {
-    audio.play()
-    alina.classList.remove("hidden")
-    alina.classList.add("visible")
-})
